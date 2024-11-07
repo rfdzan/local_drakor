b6524e2b690226948b90e56fc3e943072697dc32
heko <91366057+hkohko@users.noreply.github.com>
2024-11-07 16:25:11 +0700

    json link format completed, page shows title and links

--- src/temp_drakor/main.py
+++ src/temp_drakor/main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World<p>"
+def front_page():
+    links_dictionary = get_links()
+    return render_template("base.html.jinja", links=links_dictionary)
+
+
+def get_links() -> list[dict[str, list[dict[str, str]]]]:
+    with open("links.json", "r") as links:
+        return json.load(links)
+
+
+if __name__ == "__main__":
+    print(get_links())
